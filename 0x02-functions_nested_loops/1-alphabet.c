ac8823054e8af54e6d44cb44769ed5ffbcd567cd
easydigs <easydigitalkoncept@gmail.com>
2023-10-10 21:21:09 -0700

    I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Function to print alphabet in lower case
+ *
+ * Return: 0 on (success)
+*/
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
