672fa33c84f7c78710c070b5a8a0af350d454462
easydigs <easydigitalkoncept@gmail.com>
2023-10-12 07:18:37 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that sum two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ *
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
