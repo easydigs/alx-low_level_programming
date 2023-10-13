0ee571fc418b6969332a52339eb65708ca60706a
easydigs <easydigitalkoncept@gmail.com>
2023-10-13 12:45:52 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,13 @@
  *
  * Return: void
  */
-
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 	}
 	_putchar('\n');
 }
