2253df6e55563a2c32948fde22427ead4cd69500
easydigs <easydigitalkoncept@gmail.com>
2023-11-09 19:27:00 -0800

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,21 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c, ...);
+
+typedef struct print
+{
+	char *symbol;
+	void (*print_func)(va_list arg);
+} func_printer;
 
 #endif
