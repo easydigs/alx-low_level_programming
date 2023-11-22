e1b99ac3f8b782f711c4a98d04634bc9ce1ade68
easydigs <easydigitalkoncept@gmail.com>
2023-11-22 09:36:15 -0800

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *current;
 	unsigned int index;
 
-	if (idx == 0 || (*head == NULL && idx != 0))
+	if (idx == 0)
 	{
 		new_node = malloc(sizeof(listint_t));
 		if (new_node == NULL)
