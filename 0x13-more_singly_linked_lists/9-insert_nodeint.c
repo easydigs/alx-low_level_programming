550a6a671e9c3bb948529909857cf47ac2f2fcb4
easydigs <easydigitalkoncept@gmail.com>
2023-11-22 09:22:39 -0800

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,30 +16,33 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *current;
 	unsigned int index;
 
+	if (idx == 0 || (*head == NULL && idx != 0))
+	{
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
+
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
 	current = *head;
 
 	new_node = malloc(sizeof(listint_t));
-	if ((*head == NULL && idx != 0) || new_node == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
 
 	for (index = 0; current != NULL && index < idx - 1; index++)
-	{
 		current = current->next;
-		if (current == NULL)
-			return (NULL);
-	}
 
-	if (idx == 0)
+	if (idx == 0 || (current != NULL && current->next))
 	{
-		new_node->next = *head;
-		*head = new_node;
-	}
-	else if (current->next)
-	{
-		new_node->next = current->next;
-		current->next = new_node;
+		new_node->next = (idx == 0) ? *head : current->next;
+		(idx == 0) ? (*head = new_node) : (current->next = new_node);
 	}
 	else
 	{
