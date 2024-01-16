d736a2e9409364d4492af4ac746593b055a5e6e2
easydigs <easydigitalkoncept@gmail.com>
2024-01-16 15:03:55 +0100

    mole update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://raw.github.com/easydigs/alx-low_level_programming/master/0x18-dynamic_libraries/libmole.so
-export LD_PRELOAD="$PWD/ ../libmole.so"
+export LD_PRELOAD="$PWD/../libmole.so"
