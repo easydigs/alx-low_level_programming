919c2cce0281962dfe99819aef305cca5141993f
easydigs <easydigitalkoncept@gmail.com>
2024-05-08 17:08:16 +0100

    Task update

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -3,10 +3,10 @@
 /**
  * interpolation_search - searches for a value in an array of
  * integers using the Interpolation search algorithm
+ *
  * @array: input array
  * @size: size of the array
  * @value: value to search in
- * 
  * Return: index of the number
  */
 int interpolation_search(int *array, size_t size, int value)
